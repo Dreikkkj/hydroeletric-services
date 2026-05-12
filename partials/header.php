670227c9b02426fd7bfa3c6f5ee547171a2ba712
Dreikkj <andreiluna71@gmail.com>
2026-05-12 17:00:43 -0300

    organizando pastas

---
+++ partials/header.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="conteiner">
+        <div class="logo">
+            <h1>Hydroeletric Services</h1>
+            <img src="assets/img/" alt="Logo Hydroeletric Services">
+        </div>
+    </div>
+</header>
