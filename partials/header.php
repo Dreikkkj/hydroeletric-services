6cbb1824370466ad87e8e88f85aaefc84f5fb2dd
Dreikkj <andreiluna71@gmail.com>
2026-05-13 15:09:34 -0300

    feat: Add CRUD operations and update assets

--- partials/header.php
+++ partials/header.php
@@ -2,7 +2,7 @@
     <div class="conteiner">
         <div class="logo">
             <h1>Hydroeletric Services</h1>
-            <img src="assets/img/" alt="Logo Hydroeletric Services">
+            <img src="assets/icons/Logo.png" alt="Logo Hydroeletric Services">
         </div>
     </div>
 </header>
