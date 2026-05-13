23f6552d06eb530620a47bee0f9f3080b846eb58
Dreikkj <andreiluna71@gmail.com>
2026-05-13 17:01:20 -0300

    add font-family "Oswald" to style.css

--- index.php
+++ index.php
@@ -4,18 +4,13 @@
     <title>Página Inicial</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <!--Fontes-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
-
-    <!--CSS-->
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
 
 </body>
-
 </html>
