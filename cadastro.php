7edd22534fb8a21ef7516f2fd5f82ed5a7cd8719
seila32 <vitoroliveiradesa9@gmail.com>
2026-05-15 14:20:35 -0300

    refactor: restructure HTML and create new CSS files

--- cadastro.php
+++ cadastro.php
@@ -1,16 +1,11 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-    <title>Página Inicial</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--Fontes-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
 </head>
 <body>
-
+    
 </body>
 </html>
