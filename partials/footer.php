db2a767966c2827c4d8851f842358768c72b5023
seila32 <vitoroliveiradesa9@gmail.com>
2026-05-19 15:05:33 -0300

    Refactor header and footer structure; remove produtos.html and create produtos.php
    - Updated header.php to enhance navigation and search functionality.
    - Deleted produtos.html and replaced it with produtos.php for better integration with PHP.
    - Added footer styles in footer.css for improved design consistency.
    - Created header styles in header.css to manage header layout and aesthetics.
    - Implemented product display structure in produtos.php with filtering options.

---
+++ partials/footer.php
@@ -0,0 +1,72 @@
+<footer>
+    <div class="barraFooter">
+        <div class="parteFooter">
+            <div class="TituloFooter">
+                <img src="assets/icons/Icon_logo.png" alt="Logo da empresa">
+                <h3>HydroEletric</h3>
+            </div>
+            <a href="#">Sobre nós</a>
+            <p>Colocar o sobre a empresa, em resumo.</p>
+            <a href="./website/paginas/cadastro/termos.html">Termos e condições</a>
+        </div>
+
+        <div class="linhaFooter"></div>
+
+        <div class="parteFooter">
+            <h3>Links</h3>
+            <ul style="list-style: none;">
+                <li><a href="#">home</a></li>
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="#">catálogo</a></li>
+            </ul>
+        </div>
+
+        <div class="linhaFooter"></div>
+
+        <div class="parteFooter">
+            <h3>Contato</h3>
+            <ul style="list-style: none;">
+                <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
+                    </svg>
+                    Rua Exemplo, 123 - São Paulo, SP
+                </li>
+                <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                        <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+                    </svg>
+                    contato@HydroEletric.com.br
+                </li>
+                <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                    </svg>
+                    (11) 93056-9806
+                </li>
+                <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-fill" viewBox="0 0 16 16">
+                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5h16V4H0V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5"/>
+                    </svg>
+                    Seg-Sex, das 9h às 18h <br> Sab: 9h às 13h
+                </li>
+            </ul>
+        </div>
+
+        <div class="linhaFooter"></div>
+        
+        <div class="parteFooter">
+            <h3>Redes Sociais</h3>
+            <ul style="list-style: none;">
+                <li><a href="#">LinkedIn</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Instagram</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="DireitosReservados">
+        <p>Copyright © 2026 HydroEletric. Todos os direitos reservados.</p>
+    </div>
+</footer> 
--- produtos.php
+++ produtos.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos</title>
-    <link rel="stylesheet" href="./CSS/produtos.css">
+    <link rel="stylesheet" href="CSS/produtos.css">
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/footer.css">
 </head>
 <body>
     <header>
