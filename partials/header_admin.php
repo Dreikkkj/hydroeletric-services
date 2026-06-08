1e2ceb8e6798e25ea540255d78ae33eca3074bc4
Dreikkj <andreiluna71@gmail.com>
2026-06-08 17:00:33 -0300

    update header_admin.php and dashboard.php for admin panel navigation and styling

--- partials/header_admin.php
+++ partials/header_admin.php
@@ -1,11 +1,11 @@
 <header class="header-admin">
         <nav>
             <div class="sites">
-                <a href=""><i class="bi bi-grid"></i>Dashboard</a>
-                <a href=""><i class="bi bi-archive"></i> Estoque</a>
-                <a href=""><i class="bi bi-graph-up"></i> Financeiro</a>
-                <a href=""><i class="bi bi-gear"></i> Configurações</a>
-                <a href=""><i class="bi bi-shop-window"></i> Ver Loja</a>
+                <a href="dashboard.php"><i class="bi bi-grid"></i>Dashboard</a>
+                <a href="estoque.php"><i class="bi bi-archive"></i> Estoque</a>
+                <a href="financeiro.php"><i class="bi bi-graph-up"></i> Financeiro</a>
+                <a href="configuracoes.php"><i class="bi bi-gear"></i> Configurações</a>
+                <a href="ver_loja.php"><i class="bi bi-shop-window"></i> Ver Loja</a>
             </div>
             <div class="user">
                 <div class="adm">
