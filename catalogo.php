c7d9acaf5cdecbdee31040aede5812498ffa4e75
Breno <brenin.bacic@gmail.com>
2026-06-08 14:33:49 -0300

    refactor: Replace header and footer with partials for better code organization

--- catalogo.php
+++ catalogo.php
@@ -5,36 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
     <link rel="stylesheet" href="CSS/catalogo.css">
+    <link rel="stylesheet" href="CSS/global.css">
     <title>Document</title>
 </head>
 <body>
-    <header>
-        <nav>
-            <div>
-               <img src="Images/logo2 (1).png" alt="Logo"> 
-            </div>   
-            
-            <div class="search-header">
-                <input type="text" placeholder="Pesquisar...">
-                <i class="bi bi-search"></i>
-            </div>
-            
-            <ul>
-                <li class="header-itens"><a href="#">Home</a></li>
-                <li class="header-itens"><a href="#">Produtos</a></li>
-                <li class="header-itens"><a href="#">Contato</a></li>
-                <li class="header-cart">
-                    <a href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
-                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
-                        </svg>
-                    </a>
-                </li>
-                <li class="btn-promocao"><a href="#">Promoções</a></li>
-                <li class="btn-entrar"><a href="#">Entrar</a></li>
-            </ul>
-        </nav>
-    </header>
+    <?php require_once 'partials/header.php'; ?>
 
     <main class="main-promocao">
         <div class="text-promocao">
@@ -285,5 +260,7 @@
             </div>
         </div>
     </main>
+
+    <?php require_once 'partials/footer.php'; ?>
 </body>
 </html>
