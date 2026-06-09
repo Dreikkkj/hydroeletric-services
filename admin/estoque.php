a12aab2e6c76afcf2de4e81688e659c6458acf7d
kyyrass <kyyaa4hyy@gmail.com>
2026-06-09 14:39:56 -0300

    Update stylesheet link for admin header

--- admin/estoque.php
+++ admin/estoque.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>estoque</title>
-    <link rel="stylesheet" href="CSS/header_adm.css">
+    <link rel="stylesheet" href="CSS/header_admin.css">
     <link rel="stylesheet" href="CSS/estoque.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
 </head>
