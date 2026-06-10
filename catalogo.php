497db95ca71fd98d04db9b2a177359051a9da85d
vitor.o <vitoroliveirade575@gmail.com>
2026-06-09 22:00:58 -0300

    arrumei os titles

--- catalogo.php
+++ catalogo.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
     <link rel="stylesheet" href="CSS/catalogo.css">
     <link rel="stylesheet" href="CSS/global.css">
-    <title>Document</title>
+    <title>Catálogo</title>
 </head>
 <body>
     <?php
--- contato.php
+++ contato.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
     <link rel="stylesheet" href="CSS/contato.css">
     <link rel="stylesheet" href="CSS/global.css">
-    <title>Document</title>
+    <title>Contato</title>
 </head>
 <body>
     <?php require_once 'partials/header.php'; ?>
