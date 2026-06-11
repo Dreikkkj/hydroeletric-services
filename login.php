02d55bd0a991bc8bacc846afa9e468310293e61e
kirasz <kyyaa4hyy@gmail.com>
2026-06-11 15:12:39 -0300

    login logo

--- login.php
+++ login.php
@@ -19,6 +19,9 @@ if (isset($_GET['error'])) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
 </head>
 <body>
+    <a href="index.php" class="h">
+        <img src="assets/icons/icon_logo.png">
+    </a>
     <main>
         <section>
             <?php if (!empty($erro)): ?>
