7a853436ff0e86837d6b4ce5c68237a7b78452c4
seila32 <vitoroliveiradesa9@gmail.com>
2026-06-11 16:00:31 -0300

    .

--- partials/header_admin.php
+++ partials/header_admin.php
@@ -5,14 +5,14 @@
                 <a href="estoque.php" class="<?= $pagina == 'estoque' ? 'active' : '' ?>"><i class="bi bi-archive"></i> Estoque</a>
                 <a href="financeiro.php" class="<?= $pagina == 'financeiro' ? 'active' : '' ?>"><i class="bi bi-graph-up"></i> Financeiro</a>
                 <a href="configuracoes.php" class="<?= $pagina == 'configuracoes' ? 'active' : '' ?>"><i class="bi bi-gear"></i> Configurações</a>
-                <a href="ver_loja.php" class="<?= $pagina == 'ver_loja' ? 'active' : '' ?>"><i class="bi bi-shop-window"></i> Ver Loja</a>
+                <a href="../produtos.php"><i class="bi bi-shop-window"></i> Ver Loja</a>
             </div>
             <div class="user">
                 <div class="adm">
                     <i class="bi bi-person"></i>
                     <p>admininistrador</p>
                 </div>
-                <a href="../logout.php" class="s">sair</a>
+                <a href="../logout.php" class="s">Sair</a>
             </div>
         </nav>
 </header>
